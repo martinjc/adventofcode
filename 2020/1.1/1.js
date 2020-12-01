01361e526f1bf2f1f7811d231a317f476c707164
Martin Chorley <chorleymj@cardiff.ac.uk>
2020-12-01 14:28:09 +0000

    callback version

--- 2020/1.1/1.js
+++ 2020/1.1/1.js
@@ -1,5 +1,17 @@
 const fs = require('fs')
 
+let findNumbersMultiple = (nums, callback) => {
+
+    // look through the array, 
+    // for each number n see if (2020-n) is present. 
+    // If so, multiply numbers together and that's our answer
+    nums.forEach(n => {
+        if(nums.includes(2020-n)) {
+            callback(n * (2020-n));
+        }
+    });
+};
+
 // open the input file with the input data
 fs.readFile('input', 'utf-8', (error, data) => {
     // convert into an array for easier processing, 
@@ -9,14 +21,11 @@ fs.readFile('input', 'utf-8', (error, data) => {
     // convert each entry into a number
     nums = nums.map(n => {
         return +n;
-    })
+    });
+
+    findNumbersMultiple(nums, answer => {
+        console.log(answer);
+    });
     
-    // look through the array, 
-    // for each number n see if (2020-n) is present. 
-    // If so, multiply numbers together and that's our answer
-    nums.forEach(n => {
-        if(nums.includes(2020-n)) {
-            console.log(n * (2020-n));
-        }
-    })
+
 });
