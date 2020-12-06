b055cf860a2e722aceec70a0e5162e32eae7b7d6
Martin Chorley <martin.chorley@gmail.com>
2020-12-06 23:35:18 +0000

    2015 day 1 task 1

---
+++ 2015/1.1/lists.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+fs.readFile('input', 'utf-8', (err, data) => {
+    let floor = 0;
+    for(let i = 0; i < data.length; i++) {
+        if(data[i] === '(') {
+            floor++;
+        } else if (data[i] === ')') {
+            floor--;
+        }
+    }
+    console.log(floor);
+});
