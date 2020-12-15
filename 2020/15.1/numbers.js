12cbe06801ca09a905980965b446e245a61600ef
Martin Chorley <martin.chorley@gmail.com>
2020-12-15 09:29:42 +0000

    day 15 task 1

---
+++ 2020/15.1/numbers.js
@@ -0,0 +1,36 @@
+const { start } = require("repl");
+
+let starting_numbers = [5,2,8,16,18,0,1];
+
+let memory = {};
+
+for(let i = 0; i < starting_numbers.length; i++) {
+    memory[starting_numbers[i]] = [i+1];
+    console.log(starting_numbers[i]);
+}
+
+let last_number = starting_numbers[starting_numbers.length-1];
+
+for(let i = starting_numbers.length; i < 2020; i++) {
+    if(memory[last_number]) {
+        if(memory[last_number].length === 1) {
+            let speak = 0;
+            last_number = speak;
+            if(memory[last_number]) {
+                memory[speak].push(i+1);
+            } else {
+                memory[speak] = [i+1]
+            }
+        } else {
+            let number_spoken = memory[last_number].length;
+            let speak = memory[last_number][number_spoken-1] - memory[last_number][number_spoken-2];
+            if(memory[speak]) {
+                memory[speak].push(i+1);
+            } else {
+                memory[speak] = [i+1];
+            }
+            last_number = speak;
+        }
+        console.log(last_number);
+    }
+}
