c5507caa08b2c2870f6b013093b879cec4518302
Martin Chorley <martin.chorley@gmail.com>
2020-12-18 20:01:37 +0000

    day 18 task 1

---
+++ 2020/18.1/maths.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+
+let expression = `5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))`;
+
+const brackets_re = /(\(\d+\s[+*/-]\s\d+(\s[+*/-]\s\d+)*\))/g;
+
+
+let doOperation = function(opers) {
+    let value = 0;
+    if(opers[1] === '+') {
+        value = +opers[0] + +opers[2];
+    } else if(opers[1] === '-') {
+        value = +opers[0] - +opers[2];
+    } else if(opers[1] === '*') {
+        value = +opers[0] * +opers[2];
+    } else if(opers[1] === '/') {
+        value = +opers[0] / +opers[2];
+    }
+    return value;
+}
+
+let removeBrackets = function(stringExpression) {
+    let brackets = stringExpression.matchAll(brackets_re);
+    let b = brackets.next();
+    while(!b.done) {
+        stringExpression = stringExpression.replace(b.value[0], consumeExpression(b.value[1].replace('(', '').replace(')', '')));
+        b = brackets.next();
+    }
+    return stringExpression;
+}
+
+let consumeExpression = function(expression) {
+    let opers = expression.split(' ');
+    while(opers.length > 1) {
+        let value = doOperation(opers.slice(0,3));
+        opers = opers.slice(3);
+        opers.unshift(value);
+    }
+    return opers[0];
+}
+
+let total = 0;
+
+
+fs.readFile('input', 'utf-8', (err, data) => {
+    lines = data.split(/\r?\n/);
+    lines.forEach(l => {
+
+        console.log(l)
+        
+        while(l.includes('(') || l.includes(')')) {
+            l = removeBrackets(l);
+        }
+        l = consumeExpression(l);
+        console.log(l);
+
+        total += +l;
+    })
+    console.log(total);
+
+});
+
+
+
+
