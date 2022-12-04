9ca2fcef8c2d58f9407420c2b4342be7f160152b
Martin Chorley <martin.chorley@gmail.com>
2022-12-04 20:29:51 +0000

    today

---
+++ 2022/4/4.2.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+let input = fs.readFileSync('input', 'utf-8').split('\n');
+
+let count = 0;
+
+for(let line of input) {
+    let pair1 = line.split(',')[0];
+    let pair2 = line.split(',')[1];
+
+    let p1start = +pair1.split('-')[0];
+    let p2start = +pair2.split('-')[0];
+    let p1end = +pair1.split('-')[1];
+    let p2end = +pair2.split('-')[1];
+
+    if (p2start <= p1end && !(p2end < p1start)) {
+        count++;
+    } else if(p1start <= p2end && !(p1end < p2start)) {
+        count++;
+    }
+}
+
+console.log(count);
