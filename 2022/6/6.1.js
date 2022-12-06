a3968bed7d5687ae7f4bc6e289d31a80022a3577
Martin Chorley <martin.chorley@gmail.com>
2022-12-06 06:16:25 +0000

    today

---
+++ 2022/6/6.1.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+let input = fs.readFileSync('input', 'utf-8').split('\n');
+//let input = ['nppdvjthqldpwncqszvftbrmjlhg'];
+
+function checkMatches(chars) {
+    for(let i = 0; i < chars.length; i++) {
+        for(let j = i+1; j < chars.length; j++) {
+            if(chars[i] === chars[j]) {
+                return true
+            }
+        }
+    }
+    return false;
+}
+
+
+
+for(line of input) {
+    let chars = line.split('');
+    for(let i = 0; i < chars.length; i++) {
+        if(!checkMatches(chars.slice(i,i+4))) {
+            console.log(i+4);
+            break;
+        }
+    }
+}
