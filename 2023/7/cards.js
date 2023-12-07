a9831ec3627da9172bf77529568faec40ee6cdb4
Martin Chorley <martin.chorley@gmail.com>
2023-12-07 21:26:57 +0000

    day 7 part 2 working

--- 2023/7/cards.js
+++ 2023/7/cards.js
@@ -18,55 +18,59 @@ class Hand {
     constructor(cards, bid) {
         this.cards = cards;
         this.bid = bid;
-        this.counts = {};
+        this.counts = [];
+
         CARDS.forEach(card => {
             let matches = this.cards.match(new RegExp(card, 'g'))
             if(matches !== null) {
-                this.counts[card] = matches;
-            } else {
-                this.counts[card] = [];
+                this.counts.push([card, matches.length]);
             }
         });
+
         this.newCards = this.cards;
-        if(this.counts['J'].length > 0) {
-            let max = Math.max(...Object.values(this.counts).map(c => c.length));
-            if(max === 5) {
-                if(this.counts['J'].length === max) {
-                    this.counts['J'] = [];
-                    this.counts['A'] = ['A', 'A', 'A', 'A', 'A'];
-                    this.newCards = 'AAAAA';
-                }
-            } else if (this.counts['J'].length === max) {
-                for(let i = CARDS.length-1; i >= 0; i--) {
-                    if(this.counts[CARDS[i]].length > 0) {
-                        for(let j = 0; j < max; j++) {
-                            this.counts[CARDS[i]].push(CARDS[i]);
-                            this.counts['J'].pop();
-                            this.newCards = this.newCards.replace('J', CARDS[i]);
-                        }
-                        break;
-                    }
-                } 
+
+        this.counts.sort((a, b) => {
+            if(b[1] === a[1]) {
+                return CARDS.indexOf(b[0]) - CARDS.indexOf(a[0]);
             } else {
-                for(let i = CARDS.length-1; i >= 0; i--) {
-                    if(this.counts[CARDS[i]].length === max) {
-                        for(let j = 0; j < max; j++) {
-                            this.counts[CARDS[i]].push(CARDS[i]);
-                            this.counts['J'].pop();
-                            this.newCards = this.newCards.replace('J', CARDS[i]);                        
-                        }
-                        break;
-                    }
-                }
+                return b[1] - a[1];
             }
+        });
+
+        if(this.counts[0][0] === 'J') {
+            if(this.counts[0][1] === 5) {
+                this.newCards = 'AAAAA';
+            } else {
+                this.newCards = this.newCards.replaceAll(new RegExp('J', 'g'), this.counts[1][0]);
+            }
+        } else {
+            this.newCards = this.newCards.replaceAll(new RegExp('J', 'g'), this.counts[0][0]);
         }
-        this.pattern = Object.values(this.counts).filter(c => c.length > 0).sort((a, b) => b.length - a.length).reduce((a, b) => a + b.length, '');
+
+        this.counts = [];
+
+        CARDS.forEach(card => {
+            let matches = this.newCards.match(new RegExp(card, 'g'))
+            if(matches !== null) {
+                this.counts.push([card, matches.length]);
+            }
+        });
+
+        this.counts.sort((a, b) => {
+            if(b[1] === a[1]) {
+                return CARDS.indexOf(b[0]) - CARDS.indexOf(a[0]);
+            } else {
+                return b[1] - a[1];
+            }
+        });
+
+        this.pattern = this.counts.reduce((a, b) => a + b[1], '');
         this.score = TYPES[this.pattern];
         this.totalScore = 0;
     }
 
     toString() {
-        return `${this.cards} ${this.newCards} ${this.bid} ${this.score} ${this.totalScore}`;
+        return `${this.cards}\t${this.newCards}\t${this.pattern}\t${this.bid}\t${this.score}\t${this.totalScore}`;
     }
 }
 
