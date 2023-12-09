e2742a3868ab59cd160d90f51d8b0219222808b2
Martin Chorley <martin.chorley@gmail.com>
2023-12-09 12:06:27 +0000

    day 8

--- 2023/8/maps.js
+++ 2023/8/maps.js
@@ -7,23 +7,46 @@ const instructions = input[0].split('').map(i =>  i === 'L' ? 1 : 2);
 input = input.slice(2);
 console.log(instructions);
 
-let currentNode = input.find(i => i.substring(0, 3) === 'AAA').match(/[A-Z]{3}/g);
-let instructionNumber = 0;
-let instruction = instructions[instructionNumber];
-let step = 0;
+let startingNodes = input.filter(i => i[2] === 'A').map(n => n.match(/[0-9A-Z]{3}/g));
+console.log(startingNodes);
 
-while(currentNode[0] !== 'ZZZ') {
-    console.log(currentNode, instruction);
-    step++;
-    currentNode = input.find(i => i.substring(0, 3) === currentNode[instruction]).match(/[A-Z]{3}/g);
+let steps = [];
 
-    if(instructionNumber < instructions.length-1) {
-        instructionNumber++;
-    } else {
-        instructionNumber = 0;
+for(currentNode of startingNodes) {
+    console.log(currentNode);
+    let instructionNumber = 0;
+    let instruction = instructions[instructionNumber];
+    let step = 0;
+
+    while(currentNode[0][2] !== 'Z') {
+        //console.log(currentNode, instruction);
+        step++;
+        currentNode = input.find(i => i.substring(0, 3) === currentNode[instruction]).match(/[0-9A-Z]{3}/g);
+
+        if(instructionNumber < instructions.length-1) {
+            instructionNumber++;
+        } else {
+            instructionNumber = 0;
+        }
+        //console.log(instructionNumber);
+        instruction = instructions[instructionNumber];
     }
-    console.log(instructionNumber);
-    instruction = instructions[instructionNumber];
+    steps.push(step);
 }
 
-console.log(step);
+function gcd(a, b) {
+    return !b ? a : gcd(b, a % b);
+}
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b);   
+}
+
+console.log(steps);
+
+let multiple = steps[0];
+steps.forEach(function(n) {
+    multiple = lcm(multiple, n);
+});
+
+console.log(multiple);
